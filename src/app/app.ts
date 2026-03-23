fe73ca5f550bcb295d51bb7beec6a896f950b93d
secksanou0 <secksanou0201@gmail.com>
2026-03-23 14:24:33 +0000

    premier commit

---
+++ src/app/app.ts
@@ -0,0 +1,18 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Patient } from './patient/patient';
+import { Navbar } from './navbar/navbar';
+import { Sidebar } from './sidebar/sidebar';
+import { Footer } from './footer/footer';
+
+@Component({
+  selector: 'app-root',
+  standalone:true,
+  imports: [Navbar, Sidebar ,Footer ],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('my-app');
+}
+
---
+++ src/app/sidebar/sidebar.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Content } from '../content/content';
+
+@Component({
+  selector: 'app-sidebar',
+  imports: [Content],
+  templateUrl: './sidebar.html',
+  styleUrl: './sidebar.css',
+})
+export class Sidebar {
+
+}
