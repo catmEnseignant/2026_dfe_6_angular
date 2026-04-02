6e8cc86e619f6cbbe7070f60e3eec51a13c8a394
RabyNgaido <rabyngaido65@gmail.com>
2026-04-02 16:31:01 +0000

    Amélioration du système patients: ajout formulaire, correction affichage données, rectification navigation

--- src/app/form-patient/form-patient.ts
+++ src/app/form-patient/form-patient.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-form-patient',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './form-patient.html',
   styleUrl: './form-patient.css',
 })
 export class FormPatient {
+  formPatient=new FormGroup({
+    prenom : new FormControl(''),
+    nom : new FormControl(''),
+    mail : new FormControl(''),
+    age : new FormControl('')  
+  });
+
+   private http = inject(HttpClient);
+   private router = inject(Router);
+
+  inputFormPatient(){
+    console.log("tester");
+    console.log(this.formPatient.value);
+    let data = this.formPatient.value;
+    this.http.post("http://localhost:3000/patients",data).subscribe({
+      next: (res) => {
+        console.log('Données envoyées avec succès:');
+        console.log(res);
+        this.formPatient.reset();  // ✅ Vider le formulaire
+        this.router.navigate(['/patient']);  // ✅ Rediriger vers la liste
+      },
+      error: (err) => {
+        console.error('Erreur lors de l\'envoi des données:');
+        console.log(err);
+      }
+    });
+
+  
+    
+    
+  }
 
 }
