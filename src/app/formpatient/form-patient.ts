53aa414694bb8ae40574f651b7d0df9a652cdeba
Dieyna-Faye <dieynabaf741@gmail.com>
2026-04-29 13:37:53 +0000

    patient.ts

--- src/app/formpatient/form-patient.ts
+++ src/app/formpatient/form-patient.ts
@@ -1,54 +1,51 @@
+
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { Router } from 'express';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-formpatient',
-  imports: [CommonModule,ReactiveFormsModule],
+  selector: 'app-form-patient',
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './formpatient.html',
   styleUrl: './formpatient.css',
 })
 export class FormPatient {
+  formPatient = new FormGroup({
+    prenom : new FormControl(''),
+    nom : new FormControl(''),
+    email : new FormControl(''),
+    age : new FormControl('')
+  })
 
-  formPatient= new FormGroup({
-    prenom:new FormControl(''),
-    nom:new FormControl(''),
-    email:new FormControl('')
-  });
+  private http = inject(HttpClient) 
+  private route = inject(Router)
 
-  private http=inject(HttpClient);
-  private router=inject(Router)
-
-  inputFormPatient(){
-    console.log("tester");
+  inputFormPatient() {
+    console.log("Tester la methode inputform")
     console.log(this.formPatient.value)
-    this.http.post("http://localhost:3000/patients",this.formPatient).subscribe(res=>{
-      console.log("patient ajouter avec succes")
+    let data = this.formPatient.value
+    this.http.post('http://localhost:3000/patients', data).subscribe(res => {
+      console.log("Patient ajouté avec succés")
       console.log(res)
-
+      this.route.navigate(['/patient'])
+    }, err => {
+      console.log("Erreur lors de l'ajout")
+      console.log(err)
     })
-
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/app/sidebar/sidebar.ts
+++ src/app/sidebar/sidebar.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Content } from '../content/content';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [Content],
+  imports: [Content,RouterLink],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
